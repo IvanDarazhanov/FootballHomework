c609819f308b1d856051aa141174ce3b81cdaaae
ivandar <93916933+IvanDarazhanov@users.noreply.github.com>
2023-05-24 00:06:13 +0300

    Add files via upload

---
+++ FootballGame/FootballGame/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballGame
+{
+    public class Game
+    {
+        public Team Team1 { get; set; }
+        public Team Team2 { get; set; }
+        public Referee Referee { get; set; }
+        public Referee AssistantReferee1 { get; set; }
+        public Referee AssistantReferee2 { get; set; }
+        public void Goals(int minute, string player)
+        {
+            
+        }
+    }
+}
---
+++ FootballGame/FootballGame/Team.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballGame
+{
+    public class Team
+    {
+        public Coach Coach { get; set; }
+        //public List<Footballplayer> Players(int numplayers)
+        //{
+        //    if (numplayers < 11 || numplayers > 22)
+        //    {
+        //        throw new Exception();
+        //    }
+        //    else
+        //    {
+        //        return new List<Footballplayer>();
+        //    }
+        //}
+        public List<Footballplayer> Players { get; set; }
+        public double AverageAge()
+        {
+            double sumage =Players.Sum(player=>player.Age);
+            double avg = sumage / Players.Count;
+            return avg;
+        } 
+    }
+}
